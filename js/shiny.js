598415e6b6e418e434ca1e5837ca6b52c007d06b
therortex <45343780+therortex@users.noreply.github.com>
2018-11-26 13:48:44 +1100

    Update shiny.js

--- js/shiny.js
+++ js/shiny.js
@@ -8,3 +8,27 @@ $( ".btn-shiny1" ).click(function() {
   $(".shiny2").slideDown();
   return false
 });
+
+$( ".btn-shiny2" ).click(function() {
+  $(".shiny2").fadeOut();
+  $(".shiny3").slideDown();
+  return false
+});
+
+$( ".btn-shiny3" ).click(function() {
+  $(".shiny3").fadeOut();
+  $(".shiny4").slideDown();
+  return false
+});
+
+$( ".btn-shiny4" ).click(function() {
+  $(".shiny4").fadeOut();
+  $(".shiny5").slideDown();
+  return false
+});
+
+$( ".btn-shiny5" ).click(function() {
+  $(".shiny5").fadeOut();
+  $(".shiny6").slideDown();
+  return false
+});
