a4417cebe66482f4034aa99edf7b91dad54e5866
therortex <45343780+therortex@users.noreply.github.com>
2018-11-26 15:31:45 +1100

    Update shiny.js

--- js/shiny.js
+++ js/shiny.js
@@ -29,9 +29,7 @@ $( ".btn-shiny4" ).click(function() {
 
 $( ".btn-shiny5" ).click(function() {
   $("div.shiny5, span.shiny5").slideUp();
-  $("h2").animate({
-      display: inline-block
-    }, slow, "easein" ).delay( 800 );
-  $(".shiny6").slideDown();
+  $("h2").css('display', 'inline-block').delay( 800 );
+  $(".shiny6").fadeIn();
   return false
 });
