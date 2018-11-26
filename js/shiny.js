1d367103de04ec6d3ff63e0d4cbfe6f4acc90bff
therortex <45343780+therortex@users.noreply.github.com>
2018-11-26 13:48:00 +1100

    Update shiny.js

--- js/shiny.js
+++ js/shiny.js
@@ -2,3 +2,9 @@
 $( document ).ready(function() {
     $(".shiny1").fadeIn();
 });
+
+$( ".btn-shiny1" ).click(function() {
+  $(".shiny1").fadeOut();
+  $(".shiny2").slideDown();
+  return false
+});
