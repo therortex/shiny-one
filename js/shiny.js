88e3e678ba6b342618335b3d60ce16c356c0baae
therortex <45343780+therortex@users.noreply.github.com>
2018-11-26 14:23:55 +1100

    Update shiny.js

--- js/shiny.js
+++ js/shiny.js
@@ -4,31 +4,31 @@ $( document ).ready(function() {
 });
 
 $( ".btn-shiny1" ).click(function() {
-  $(".shiny1").fadeOut();
-  $(".shiny2").fadeIn();
+  $(".shiny1").slideUp();
+  $(".shiny2").slideDown();
   return false
 });
 
 $( ".btn-shiny2" ).click(function() {
-  $("div.shiny2, span.shiny2").fadeOut();
-  $(".shiny3").fadeIn();
+  $("div.shiny2, span.shiny2").slideUp();
+  $(".shiny3").slideDown();
   return false
 });
 
 $( ".btn-shiny3" ).click(function() {
-  $("div.shiny3, span.shiny3").fadeOut();
-  $(".shiny4").fadeIn();
+  $("div.shiny3, span.shiny3").slideUp();
+  $(".shiny4").slideDown();
   return false
 });
 
 $( ".btn-shiny4" ).click(function() {
-  $("div.shiny4, span.shiny4").fadeOut();
-  $(".shiny5").fadeIn();
+  $("div.shiny4, span.shiny4").slideUp();
+  $(".shiny5").slideDown();
   return false
 });
 
 $( ".btn-shiny5" ).click(function() {
-  $("div.shiny5, span.shiny5").fadeOut();
-  $(".shiny6").fadeIn();
+  $("div.shiny5, span.shiny5").slideUp();
+  $(".shiny6").slideDown();
   return false
 });
